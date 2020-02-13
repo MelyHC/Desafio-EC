55793e193f18244c3c5187f36ab12ee6faa927be
Eduardo Hidalgo <eduardo.hidalgo.crespo@gmail.com>
2020-02-13 00:08:30 -0500

    Updates

---
+++ precache-manifest.d1195bc7b2ed7398506a7a17c65deaba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f28ef357e3a3bdee6462daffb446b0f",
+    "url": "/Desafio-EC/index.html"
+  },
+  {
+    "revision": "51b7982823b5a2f839d6",
+    "url": "/Desafio-EC/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b40f5d33fa861fff9139",
+    "url": "/Desafio-EC/static/css/main.b20d9bf4.chunk.css"
+  },
+  {
+    "revision": "51b7982823b5a2f839d6",
+    "url": "/Desafio-EC/static/js/2.b77c0350.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Desafio-EC/static/js/2.b77c0350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40f5d33fa861fff9139",
+    "url": "/Desafio-EC/static/js/main.a6880f98.chunk.js"
+  },
+  {
+    "revision": "1f65ed943727ff5f34cd",
+    "url": "/Desafio-EC/static/js/runtime-main.b51f292b.js"
+  }
+]);
